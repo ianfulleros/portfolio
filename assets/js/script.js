71dfe73d2de636c824a8ba1c0f1cb4a7058ba8cc
ianfulleros <ianfulleros015@gmail.com>
2018-06-27 12:09:48 +0800

    my portfolio

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+	console.log('kjdhskad');
+
+	$('#main-wrapper').fullpage({
+		scrollingSpeed:700,
+		sectionsColor:['orange','#000','#28023D','#fff','#FF5F45','lavender'],
+		scrollBar:false,
+		navigation:true,
+		slidesNavigation:true,
+		controlArrows:false,
+		loopHorizontal:true
+		
+
+	});
+
+
+
+
+});
